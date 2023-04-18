4d03e4af8be301333423f4426cd1504e9cbe7ab7
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-04-18 21:35:54 +0200

    5

--- sql-exercises/Ejercicio.sql
+++ sql-exercises/Ejercicio.sql
@@ -1,23 +1,29 @@
--- Devuelve toda la información de la tabla laureates.
+--1. Devuelve toda la información de la tabla laureates.
 SELECT * FROM "laureates";
 
--- Devuelve toda la información de la tabla nobels
+--2. Devuelve toda la información de la tabla nobels
 SELECT * FROM "nobels";
 
--- Devuelve toda la información relativa al Nobel de Química.
+--3. Devuelve toda la información relativa al Nobel de Química.
 SELECT * FROM "nobels" WHERE category = 'Chemistry';
 
--- Devuelve el nombre completo del Nobel de Ciencias Económicas.
+--4. Devuelve el nombre completo del Nobel de Ciencias Económicas.
 SELECT category_fullname FROM "nobels" WHERE category = 'Economic Sciences';
 
--- Devuelve el nombre completo y el `award_year` de los Nobel dados en el año 2019.
+--5. Devuelve la motivación y la categoría del Nobel de la Paz.
+SELECT category, motivation FROM "nobels" where category = 'Peace';
+
+--6. Devuelve el nombre completo y el `award_year` de los Nobel dados en el año 2019.
 SELECT award_year,category_fullname FROM "nobels" WHERE award_year = 2019;
 
--- Devuelve el la categoría y el `award_year` de los Nobel dados entre las fechas: 
+--7. Devuelve el la categoría y el `award_year` de los Nobel dados entre las fechas: 
 -- - 12 de octubre de 2007
 -- - 11 de octubre de 2019
 
 SELECT award_year,category_fullname FROM "nobels" WHERE award_year  BETWEEN 2007 and 2019;
+
+
+
 
 
 
