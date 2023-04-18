292e0c82da99f689d4f5b3edbdf5f14c61c16bb7
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-04-18 22:39:13 +0200

    nobels completo

--- sql-exercises/Ejercicio.sql
+++ sql-exercises/Ejercicio.sql
@@ -39,8 +39,44 @@ SELECT award_year , category_fullname
 FROM "nobels" 
 WHERE category = 'Chemistry'
 ORDER BY award_year ASC;
+
 -- 9. Devuelve la categoría, la motivación y el premio de los Nobel cuyo premio sea mayor de 500000$.
+SELECT category,motivation
+FROM "nobels"
+WHERE prize_amount > 500000;
+
 -- 10. Devuelve la categoría, y el `award_year` y el premio de los Nobel cuya categoría sea Física.
+SELECT category, award_year, prize_amount 
+FROM "nobels" 
+WHERE category = 'Physics';
+-- 11. Devuelve la categoría y el `award_year` de los Nobel cuyo premio esté comprendido entre 100000$ y 300000$.
+SELECT category, award_year 
+FROM "nobels" 
+WHERE prize_amount 
+BETWEEN 100000 and 300000;
+
+-- 12. Devuelve la suma de los premios Nobel de las categorías de Paz y Literatura.
+SELECT SUM(prize_amount) AS sum_pize_amount
+FROM "nobels"
+WHERE category IN ('Peace','Literature')
+
+-- 13. Devuelve las 5 afiliaciones y categorías de los Nobel cuyos premios sean los menores.
+SELECT affiliations, category 
+FROM "nobels"
+ORDER BY prize_amount ASC 
+LIMIT 5;
+-- 14. Devuelve los 7 `award_year` más recientes en los que se ha entregado algún Nobel. Los años NO deben repetirse. Tienes que devolver 7 diferentes.
+SELECT DISTINCT award_year 
+FROM "nobels"
+WHERE prize_status = 'received'
+ORDER BY award_year DESC 
+LIMIT 7;
+
+-- 15. Devuelve `award_year`, `category_fullname` y `date_awarded` de los Nobel cuyo campo `date_awarded` sea `NULL`.
+SELECT award_year,category_fullname,date_awarded 
+FROM "nobels"
+WHERE date_awarded IS NULL;
+-- 16. Devuelve la suma de los premios de los Nobel cuyo campo `date_awarded` sea `NULL`
 
 
 
