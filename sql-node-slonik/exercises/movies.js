93212c16011e329c00ba7057bce52d5c2c32e5ed
Marcos <marcosugaldemartinez@gmail.com>
2023-04-21 21:45:38 +0200

    added the first movies queries

--- sql-node-slonik/exercises/movies.js
+++ sql-node-slonik/exercises/movies.js
@@ -1,65 +1,77 @@
-const db = require('../configs/db')
-const { sql } = require('slonik')
+const db = require("../configs/db");
+const { sql } = require("slonik");
 
 module.exports = {
-    // Your movies queries
-    async q9() {
-        // README - Movies.9
-    },
+  // Your movies queries
+  async q9() {
+    // README - Movies.9
+    return await db.query(sql`
+        SELECT title FROM movies
+        WHERE title IS NOT NULL
+        `);
+  },
 
-    async q10() {
-        // README - Movies.10
-    },
+  async q10() {
+    // README - Movies.10
+    return await db.query(sql`
+    SELECT title, mpaa_rating FROM movies
+    WHERE mpaa_rating IS NOT NULL 
+    `);
+  },
 
-    async q11() {
-        // README - Movies.11
-    },
+  async q11() {
+    // README - Movies.11
+    return await db.query(sql`
+    SELECT title, production_budget, distributor FROM movies
+    WHERE production_budget < 500000
+    `);
+  },
 
-    async q12() {
-        // README - Movies.12
-    },
+  async q12() {
+    // README - Movies.12
+  },
 
-    async q13() {
-        // README - Movies.13
-    },
+  async q13() {
+    // README - Movies.13
+  },
 
-    async q14() {
-        // README - Movies.14
-    },
+  async q14() {
+    // README - Movies.14
+  },
 
-    async q15() {
-        // README - Movies.15
-    },
+  async q15() {
+    // README - Movies.15
+  },
 
-    async q16() {
-        // README - Movies.16
-    },
+  async q16() {
+    // README - Movies.16
+  },
 
-    async q17() {
-        // README - Movies.17
-    },
+  async q17() {
+    // README - Movies.17
+  },
 
-    async q18() {
-        // README - Movies.18
-    },
+  async q18() {
+    // README - Movies.18
+  },
 
-    async q19() {
-        // README - Movies.19
-    },
+  async q19() {
+    // README - Movies.19
+  },
 
-    async q20() {
-        // README - Movies.20
-    },
+  async q20() {
+    // README - Movies.20
+  },
 
-    async q21() {
-        // README - Movies.21
-    },
+  async q21() {
+    // README - Movies.21
+  },
 
-    async q22() {
-        // README - Movies.22
-    },
+  async q22() {
+    // README - Movies.22
+  },
 
-    async q23() {
-        // README - Movies.23
-    },
-}
+  async q23() {
+    // README - Movies.23
+  },
+};
