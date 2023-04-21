4d6c56fc29fdafb1e5815c6e2e48f66ee8675fe5
Marcos <marcosugaldemartinez@gmail.com>
2023-04-21 21:37:19 +0200

    added all queries in directors

--- sql-node-slonik/exercises/directors.js
+++ sql-node-slonik/exercises/directors.js
@@ -23,27 +23,47 @@ module.exports = {
     return await db.query(sql`
     SELECT pic, nickname FROM directors
     WHERE nickname != ''    
-    `); // me devuelve algunos nicknames sin valor
+    `);
   },
 
   async q4() {
     // README - Directors.4
-    return await db.query();
+    return await db.query(sql`
+    SELECT query_name, nationality FROM directors
+    WHERE nationality = 'canadiense'
+    `);
   },
 
   async q5() {
     // README - Directors.5
+    return await db.query(sql`
+    SELECT query_name, nationality FROM directors
+    WHERE (nationality LIKE '%brit%')
+    AND (nationality LIKE '%estadoun%')
+    `);
   },
 
   async q6() {
     // README - Directors.6
+    return await db.query(sql`
+    SELECT query_name, nationality, roles FROM directors
+    WHERE roles LIKE '%ajedrecista%'
+    `);
   },
 
   async q7() {
     // README - Directors.7
+    return await db.query(sql`
+    SELECT query_name, name, nationality FROM directors
+    WHERE nationality LIKE '%,%'
+    `);
   },
 
   async q8() {
     // README - Directors.8
+    return await db.query(sql`
+    SELECT query_name, roles FROM directors
+    WHERE roles LIKE '%,%,%'
+    `);
   },
 };
