197f2274de8806713973eec1e42d995adeb48ef7
natal <nataliareyesvargas15@gmail.com>
2023-04-24 23:08:33 +0200

    directors_movies

--- sql-node-slonik/exercises/directors.js
+++ sql-node-slonik/exercises/directors.js
@@ -1,37 +1,78 @@
 const db = require('../configs/db')
 const { sql } = require('slonik')
 
+
 module.exports = {
     // Your directors queries
     async q1() {
         // README - Directors.1
+        return await db.query(sql`
+        SELECT name
+        FROM directors 
+        WHERE name <> ''
+        `);
     },
+    
 
     async q2() {
         // README - Directors.2
-    },
+        return await db.query(sql`
+        SELECT query_name, nickname
+        FROM directors 
+        `)},
+    
 
     async q3() {
         // README - Directors.3
-    },
+        return await db.query(sql`
+        SELECT pic, nickname
+        FROM directors 
+        WHERE nickname <> ''
+        `)},
+    
 
     async q4() {
         // README - Directors.4
-    },
+        return await db.query(sql`
+        SELECT query_name, nationality
+        FROM directors 
+        WHERE nationality LIKE 'canadiense'
+        `)},
 
     async q5() {
         // README - Directors.5
+        return await db.query(sql`
+        SELECT query_name, nationality
+        FROM directors 
+        WHERE nationality LIKE 'británica,estadounidense'
+        `)
     },
 
     async q6() {
         // README - Directors.6
+        return await db.query(sql`
+        SELECT query_name, nationality,roles 
+        FROM directors 
+        WHERE roles LIKE ajedrecista
+        `)
     },
 
     async q7() {
         // README - Directors.7
+        return await db.query(sql`
+        SELECT query_name, name, nationality
+        FROM directors 
+        WHERE nationality LIKE ('%,%')
+        `)
     },
 
     async q8() {
         // README - Directors.8
+        return await db.query(sql`
+        SELECT query_name, roles
+        FROM directors 
+        WHERE roles LIKE ('%,%,%')
+        `)
     },
 }
+
