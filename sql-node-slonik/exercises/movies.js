1d2f68e27d2e3d0762bc5daabcf0bf231df7aa78
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-04-26 18:15:32 +0200

    movies

--- sql-node-slonik/exercises/movies.js
+++ sql-node-slonik/exercises/movies.js
@@ -64,6 +64,7 @@ module.exports = {
 
     async q20() {
         // README - Movies.20
+        return await (sql `SELECT title, us_gross, worldwide_gross FROM movies WHERE us_gross = 0  OR worldwide_gross = 0 `)
     },
 
     async q21() {
