234b3622b54da996358d49f37c104d4292922194
paumadrii <paumadri27@outlook.com>
2023-04-26 18:33:33 +0200

    pull 1-33

--- sql-node-slonik/exercises/directors.js
+++ sql-node-slonik/exercises/directors.js
@@ -4,34 +4,67 @@ const { sql } = require('slonik')
 module.exports = {
     // Your directors queries
     async q1() {
-        // README - Directors.1
+        return await db.query(sql`
+        SELECT * FROM directors 
+        WHERE directors.name != ''
+        `)
     },
 
-    async q2() {
-        // README - Directors.2
+   async q2() {
+        return await db.query(sql`
+        SELECT query_name, nickname
+        FROM directors 
+        WHERE nickname != ''
+        `)
     },
 
     async q3() {
-        // README - Directors.3
+        return await db.query(sql`
+        SELECT pic, nickname
+        FROM directors 
+        WHERE nickname != ''
+        `)
+
     },
 
     async q4() {
-        // README - Directors.4
+        return await db.query(sql`
+        SELECT query_name, nationality
+        FROM directors 
+        WHERE nationality = 'Canadiense'
+        `)
     },
 
     async q5() {
-        // README - Directors.5
+        return await db.query(sql`
+        SELECT query_name, nationality
+        FROM directors 
+        WHERE nationality = 'estadounidense,británico'
+        `)
     },
 
     async q6() {
-        // README - Directors.6
+        return await db.query(sql`
+        SELECT query_name, nationality, roles
+        FROM directors 
+        WHERE roles= 'ajedrecista'; 
+        `)
     },
 
     async q7() {
-        // README - Directors.7
+        return await db.query(sql`
+        SELECT query_name, name, nationality
+        FROM directors 
+        WHERE nationality LIKE '%,%'
+        `)
     },
 
     async q8() {
-        // README - Directors.8
+        return await db.query(sql` 
+        SELECT query_name, roles
+        FROM directors
+        WHERE roles LIKE '%,%,%'
+
+        `)
     },
 }
--- sql-node-slonik/exercises/directors_movies.js
+++ sql-node-slonik/exercises/directors_movies.js
@@ -4,43 +4,115 @@ const { sql } = require('slonik')
 module.exports = {
     // Your joins from directors and movies queries
     async q24() {
-        // README - Directors_Movies.24
+        return db.query(sql`
+        SELECT query_name, production_budget, distributor
+        FROM directors 
+        INNER JOIN movies 
+        ON directors.id = movies.id
+        WHERE (query_name IS NOT NULL) AND (production_budget IS NOT NULL) AND  (distributor IS NOT NULL)
+        `)
     },
 
     async q25() {
-        // README - Directors_Movies.25
+        return db.query(sql`
+        SELECT query_name, COUNT(title) AS total_movies
+        FROM directors
+        INNER JOIN movies
+        ON movies.director=directors.id
+        GROUP BY directors.query_name
+        `)
     },
 
     async q26() {
-        // README - Directors_Movies.26
+        return db.query(sql`
+        SELECT query_name, title, imdb_votes
+        FROM directors
+        RIGHT JOIN movies
+        ON movies.director=directors.id
+        WHERE (query_name IS NOT NULL) AND (imdb_votes IS NOT NULL)
+        ORDER BY imdb_votes ASC
+        LIMIT 50
+        `)
     },
 
     async q27() {
-        // README - Directors_Movies.27
+        return db.query(sql`
+        SELECT query_name, distributor
+        FROM directors
+        INNER JOIN movies
+        ON movies.director=directors.id
+        WHERE directors.name= 'Christopher Nolan'
+        `)
     },
 
     async q28() {
-        // README - Directors_Movies.28
+        return db.query(sql`
+        SELECT name, MAX(us_gross) AS max_gross
+        FROM directors
+        INNER JOIN movies
+        ON directors.id=movies.director
+        WHERE (directors.name IS NOT NULL) AND (movies.us_gross IS NOT NULL)
+        GROUP BY movies.us_gross, directors.name
+        ORDER BY movies.us_gross DESC
+        LIMIT 1
+        `)
     },
 
     async q29() {
-        // README - Directors_Movies.29
+        return db.query(sql`
+        SELECT d.name, COUNT(m.release_date) AS r_d
+        FROM directors d
+        INNER JOIN movies m 
+        ON d.id=m.director
+        WHERE m.release_date BETWEEN '2000-01-01' AND CURRENT_DATE
+        GROUP BY d.name
+        ORDER BY COUNT(*) DESC
+        LIMIT 1
+        `)
     },
 
     async q30() {
-        // README - Directors_Movies.30
+        return db.query(sql`
+        SELECT name,major_genre, rotten_tomatoes_rating
+        FROM directors
+        INNER JOIN movies
+        ON directors.id=movies.director
+        WHERE  (movies.major_genre='Drama') AND (movies.rotten_tomatoes_rating >= 70)
+        `)
     },
 
     async q31() {
-        // README - Directors_Movies.31
+        return db.query(sql`
+        SELECT name
+        FROM directors
+        INNER JOIN movies
+        ON directors.id = movies.director
+        WHERE (directors.nationality = 'australiana') AND (movies.release_date <= '1995-12-12')
+        `)
     },
 
     async q32() {
-        // README - Directors_Movies.32
+        return db.query(sql`
+        SELECT d.name, m.title, m.release_date, m.mpaa_rating
+        FROM directors d
+        LEFT JOIN movies m
+        ON m.director=d.id
+        WHERE m.mpaa_rating = 'PG-13'
+        ORDER BY m.release_date ASC;
+        `)
     },
 
     async q33() {
-        // README - Directors_Movies.33
+        return db.query(sql`
+        SELECT name, AVG(imdb_rating) AS rating
+        FROM directors
+        INNER JOIN movies
+        ON directors.id=movies.director
+        WHERE (directors.nationality='canadiense') AND (movies.imdb_rating IS NOT NULL)
+        GROUP BY movies.imdb_rating, directors.name
+        ORDER BY movies.imdb_rating DESC
+        LIMIT 1 OFFSET 4;
+        `)
     },
 
     async q34() {
