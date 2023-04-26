6ffc2fa73ad46cd22ec88c41b0c9d26857029bd1
Ivan Romero <ivanromero5520@gmail.com>
2023-04-26 20:30:20 +0200

    changes acepted

--- sql-node-slonik/exercises/directors.js
+++ sql-node-slonik/exercises/directors.js
@@ -5,16 +5,57 @@ module.exports = {
     // Your directors queries
     async q1() {
         // README - Directors.1
+     const query = `
+     SELECT name 
+     FROM  directors
+     WHERE name IS NOT NULL
+     `;
+    
+     try {
+      const conection = await createPool.getConnection();
+
+        const result = await connection.query(sql`
+        SELECT name 
+        FROM  directors
+        WHERE name IS NOT NULL
+        `);
+        connection.realease();
+        return result.rows.map(row => row.name);
+
+        }   catch (error ) {
+            console.error(error)
+            throw 'error1';
+        }
+     
     },
+     
+    
+
 
     async q2() {
         // README - Directors.2
+        const query = `
+        SELECT query_name, nickname 
+        FROM directors 
+        WHERE nickname = $1 AND roles = 'director'
+        `;
+
+        try {
+            const result = await db.query(query);
+            return result.rows
+            
+        } catch (error) {
+        console.error(error)
+            throw 'error2';
+        }
     },
+     
+   
 
     async q3() {
-        // README - Directors.3
-    },
 
+    },
+        // README - Directors.3
     async q4() {
         // README - Directors.4
     },
